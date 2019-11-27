f1f26368d966fd43dc276bc823cfe82dcf7b06e9
Brandon Arfa <brandonarfa@Brandons-MacBook-Air.local>
2019-11-27 11:35:43 -0500

    Finished

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import sys
+from statistics import mean
+
+def isFloat(string):
+    try: 
+        float(string)
+        return True
+    except ValueError: 
+        return False 
+
 
 
 def main():
@@ -8,6 +17,19 @@ def main():
     print(f"Processing input file: {input_file_path}")
 
     # TODO: Fill in the actual logic here!
+
+    f = open(input_file_path, "r")
+    values = f.read()
+    values = values.split("\n")
+
+    for val in values:
+        strvals = val.split(",")
+        numvals = []
+        for nv in strvals:
+            if isFloat(nv):
+                numvals.append(float(nv))
+        if numvals != []:
+            print(mean(numvals))
 
 
 if __name__ == "__main__":
